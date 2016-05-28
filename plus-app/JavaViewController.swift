e1edd9c2b73e3a91dd067f26f1851b09039d18a1
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-05-28 02:52:36 +0200

    mvp wkwebview

--- plus-app/JavaViewController.swift
+++ plus-app/JavaViewController.swift
@@ -4,15 +4,65 @@
 //
 
 import UIKit
+import WebKit
 
-class JavaViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        self.setNavigationBarItem()
+extension WKWebView {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return nil
     }
 }
+
+class JavaViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView?
+    
+    /* Start the network activity indicator when the web view is loading */
+    func webView(webView: WKWebView,
+                 didStartProvisionalNavigation navigation: WKNavigation){
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    /* Stop the network activity indicator when the loading finishes */
+    func webView(webView: WKWebView,
+                 didFinishNavigation navigation: WKNavigation){
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
+    func webView(webView: WKWebView,
+                 decidePolicyForNavigationResponse navigationResponse: WKNavigationResponse,
+                                                   decisionHandler: ((WKNavigationResponsePolicy) -> Void)){
+        
+        print(navigationResponse.response.MIMEType)
+        
+        decisionHandler(.Allow)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Create our preferences on how the web page should be loaded */
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true
+        
+        /* Create a configuration for our preferences */
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences = preferences
+        
+        /* Now instantiate the web view */
+        webView = WKWebView(frame: view.bounds, configuration: configuration)
+        
+        if let theWebView = webView{
+            /* Load a web page into our web view */
+            let url = NSURL(string: "http://www.apple.com")
+            let urlRequest = NSURLRequest(URL: url!)
+            theWebView.scrollView.bounces = false
+            theWebView.loadRequest(urlRequest)
+            theWebView.navigationDelegate = self
+            view.addSubview(theWebView)
+            
+        }
+        
+    }
+    
+}
+
