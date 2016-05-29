1ae365fdcf4be8d08ce1554443c61451b3f2a246
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-05-29 21:28:14 +0200

    send screensize

--- plus-app/GAViewExtension.swift
+++ plus-app/GAViewExtension.swift
@@ -23,6 +23,7 @@ extension UIViewController {
     
     func trackEvent(category: String, action: String, label: String, value: NSNumber?) {
         let tracker = GAI.sharedInstance().defaultTracker
+        tracker.allowIDFACollection = true
         let trackDictionary = GAIDictionaryBuilder.createEventWithCategory(category, action: action, label: label, value: value)
         tracker.send(trackDictionary.build() as [NSObject : AnyObject])
     }
